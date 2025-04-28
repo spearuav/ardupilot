0f392707aaa63912ce4039cc5e41a520c4db2817
Eitan Amit <eitan.amit@spearuav.com>
2025-04-28 11:09:23 +0300

    Clean developer names

--- Tools/FilterTestTool/run_filter_test.py
+++ Tools/FilterTestTool/run_filter_test.py
@@ -14,7 +14,6 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-__author__ = "Guglielmo Cassinelli"
 __contact__ = "gdguglie@gmail.com"
 
 try: # Python 3.x
